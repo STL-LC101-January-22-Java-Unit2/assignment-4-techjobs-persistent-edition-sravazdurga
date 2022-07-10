9d316c01ef81ccd253339c46616317f3c35af9b5
Sravanthi <sravazdurga@gmail.com>
2022-07-10 16:32:52 -0400

    Assignment #4: Tech Jobs (Persistent Edition)

--- queries.sql
+++ queries.sql
@@ -6,27 +6,3 @@ SELECT name FROM employer WHERE location = "St. Louis City";
 DROP TABLE job;
 -- Part 4: Test it with SQL
 SELECT * from skill left join job_skills on skill.id = job_skills.skills_id where job_skills.jobs_id is not null order by name ASC;
---SELECT\\s+\\*\\s+FROM\\s+skill" +
---                "\\s*(LEFT|INNER)?\\s+JOIN\\s+job_skills\\s+ON\\s+(skill.id\\s+=\\s+job_skills.skills_id|job_skills.skills_id\\s+=\\s+skill.id)" +
---                "(\\s*WHERE\\s+job_skills.jobs_id\\s+IS\\s+NOT\\s+NULL)?" +
---                "\\s*ORDER\\s+BY\\s+name\\s+ASC;
-
---SELECT * FROM skill
---WHERE id IN (SELECT skills_id FROM job_skills WHERE skills_id IS NOT NULL)
---ORDER BY name ASC;
---
---SELECT job.name, skill.description
---FROM skill, job
---INNER JOIN job_skills ON job.id = job_skills.jobs_id
---WHERE skill.id = job_skills.skills_id
---ORDER BY job.name ASC;
---
---SELECT * FROM skill
---LEFT INNER JOIN job_skills ON skill.id = job_skills.skills_id OR job_skills.skills_id = skill.id
---WHERE job_skills.jobs_id IS NOT NULL
---ORDER BY name ASC
---
---SELECT *
---FROM  skill (LEFT INNER)
---JOIN job_skills
---ON  (skill.id = job_skills.skills_id OR job_skills.skills_id = skill.id) (WHERE job_skills.jobs_id IS NOT NULL) ORDER BY NAME ASC;
